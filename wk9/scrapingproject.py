b1dc19c88fd5e833f342dc48fdbed2a770727dfa
apsaw <apsawicki@gmail.com>
2019-04-10 18:04:57 -0400

    wk9

--- wk9/scrapingproject.py
+++ wk9/scrapingproject.py
@@ -1,3 +1,6 @@
+from bs4 import BeautifulSoup
+import requests
+
 pokemon = []
 types = ['Normal', 'Fire', 'Water', 'Electric', 'Grass', 'Ice', 'Fighting', 'Poison', 'Ground', 'Flying',
          'Psychic', 'Bug', 'Rock', 'Ghost', 'Dragon', 'Dark', 'Steel', 'Fairy']
